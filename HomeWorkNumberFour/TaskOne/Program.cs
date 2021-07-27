23e8a50f4e691be5e70a04fdf77f94e7d784dbe1
Andrei Vovchok <andreivovchok@bk.ru>
2021-07-27 22:36:54 +0300

    Добавил решение первого задания

---
+++ HomeWorkNumberFour/TaskOne/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TaskOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите число от 0 до 100");
+            string s = Console.ReadLine();
+            int numbers = int.Parse(s);
+
+
+            
+            if(numbers >= 0 && numbers <= 30)
+            {
+                Console.WriteLine("Вы в первом промежутке!");
+            }
+            else if(numbers >= 31 && numbers <= 60)
+            {
+                Console.WriteLine("Вы во втором промежутке!");
+            }
+            else if(numbers >= 61 && numbers <= 100)
+            {
+                Console.WriteLine("Вы в третьем промежутке");
+            }
+            else
+            {
+                Console.WriteLine("Я не знаю такого числа!");
+            }
+            
+        }
+    }
+}
